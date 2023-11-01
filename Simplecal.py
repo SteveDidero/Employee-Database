43fc8c5005588e98d9ea03af8c95d9be4f0364af
Gene <gene.y.ziel@gmail.com>
2023-10-31 20:55:22 -0400

    Made simpleCal() return the sum of the args

--- Simplecal.py
+++ Simplecal.py
@@ -3,14 +3,9 @@ def simpleCal (x , y):
     A simple calculator that takes two numbers and return the sum.
 
     Args:
-        X: the first nummber
+        x: the first nummber
         y: the second number
-    Returns:
-    """    
-    x = 1    
-    y = 2
     
-
-    def __init__ (self, x, y):
-        self.x = x
-        self.y = y
+    Returns:
+    """
+    return x + y
