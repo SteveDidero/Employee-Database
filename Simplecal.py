1e3c5ad8b1d6e80807dfb3b3dff59784bbf9521c
Steve Tanekeu <Stanekeu@umd.edu>
2023-10-31 20:26:20 -0400

    defined a simple calculator function and added dogstrings

---
+++ Simplecal.py
@@ -0,0 +1,9 @@
+def simpleCal (x , y):
+    """
+    A simple calculator that takes two numbers and return the sum.
+
+    Args:
+        X: the first nummber
+        y: the second number
+    """
+    
