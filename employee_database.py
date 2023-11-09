8009b46a332a2f6e178efe651e671ccd8c68378a
Gene <gene.y.ziel@gmail.com>
2023-11-09 17:35:46 -0500

    Added type and value checks to the Employee method __init__(). Also added the Employee copying feature to __init__().

--- employee_database.py
+++ employee_database.py
@@ -11,6 +11,8 @@ the Free Software Foundation, either version 3 of the License, or
 # INST326 section 0101
 # Team: Pythonista
 
+BAD_VALUES = frozenset({None, ""})
+
 class Employee:
     """The personal information of an employee at a company.
     
@@ -25,6 +27,34 @@ class Employee:
             "address": (str) Home address.
             "position": (str) The employee's job.
             "department": (str) The employee's department.
-            "salary": (int) Annual gross salary. Non-negative.
+            "salary": (int) Annual gross salary.
         }
     """
+    
+    def __init__(self, employee, name="", gender="", dob="", email="", phone=""
+            , address="", position="", department="", salary=-1):
+        """Initializes the employee's record.
+        
+        Args:
+            employee: Either an employee ID (int) or an Employee object. The
+                following args must be given if the employee arg is an ID.
+            "name": (str)
+            "gender": (str) Either "m"an, "w"oman, or "n"onbinary.
+            "dob": (str) Date of birth.
+            "email": (str) Email address.
+            "phone": (str) Phone number.
+            "address": (str) Home address.
+            "position": (str) The employee's job.
+            "department": (str) The employee's department.
+            "salary": (int) Annual gross salary.
+        """
+        if not isinstance(employee, (Employee, int)):
+            raise TypeError("The employee arg should be an Employee or an int.")
+        if isinstance(employee, int) and ({name, gender, dob, email, phone
+                , address, position, department, salary} & BAD_VALUES):
+            raise ValueError("If an ID is given then the other attributes must"
+                "be non-empty.")
+        if isinstance(employee, Employee):
+            self.info = employee.info.copy()
+            return
+        
