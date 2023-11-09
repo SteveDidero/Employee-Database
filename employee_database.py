af72e89ea8badf3d9006170f9367b26b7b3a8180
Gene <gene.y.ziel@gmail.com>
2023-11-09 16:38:26 -0500

    Created the main code file and added the license header.

---
+++ employee_database.py
@@ -0,0 +1,12 @@
+"""Classes representing a database of employees and managers in a company.
+
+Copyright (C) 2023
+Jordan Goodman, Trinity Hill, Spencer Morgan, Steve Tanekeu, Gene Yu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+"""
+# INST326 section 0101
+# Team: Pythonista
