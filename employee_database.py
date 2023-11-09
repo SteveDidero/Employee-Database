27d5d7ed9586a374a47b494a5983ce2695063c89
Gene <gene.y.ziel@gmail.com>
2023-11-09 18:59:14 -0500

    Moved employee ID from the Employee class to the Company class. Changed Employee class docstrings accordingly. Created a placeholder of the Company class with methods __init__(), add_employee(), and write_employees_json().

--- employee_database.py
+++ employee_database.py
@@ -14,12 +14,13 @@ the Free Software Foundation, either version 3 of the License, or
 BAD_VALUES = frozenset({None, ""})
 
 class Employee:
-    """The personal information of an employee at a company.
+    """Represents the personal information of an employee at a company.
+    
+    Primary author: ?
     
     Attributes:
         info (dict): A dictionary containing the following key-value pairs {
-            "id": (int) The employee's ID at the company. Should be unique.
-            "name": (str)
+            "name": (str) The employee's name.
             "gender": (str) Either "m"an, "w"oman, or "n"onbinary.
             "dob": (str) Date of birth.
             "email": (str) Email address.
@@ -31,26 +32,30 @@ class Employee:
         }
     """
     
-    def __init__(self, employee, name="", gender="", dob="", email="", phone=""
+    def __init__(self, employee, gender="", dob="", email="", phone=""
             , address="", position="", department="", salary=-1):
         """Initializes the employee's record.
         
+        Primary author: Gene Yu
+        
         Args:
-            employee: Either an employee ID (int) or an Employee object. The
-                following args must be given if the employee arg is an ID.
-            "name": (str)
-            "gender": (str) Either "m"an, "w"oman, or "n"onbinary.
-            "dob": (str) Date of birth.
-            "email": (str) Email address.
-            "phone": (str) Phone number.
-            "address": (str) Home address.
-            "position": (str) The employee's job.
-            "department": (str) The employee's department.
-            "salary": (int) Annual gross salary.
+            employee: Either a name (str) or an Employee object. The following
+                args must be given if the employee arg is a name.
+            gender: (str) Either "m"an, "w"oman, or "n"onbinary.
+            dob: (str) Date of birth.
+            email: (str) Email address.
+            phone: (str) Phone number.
+            address: (str) Home address.
+            position: (str) The employee's job.
+            department: (str) The employee's department.
+            salary: (int) Annual gross salary.
+        
+        Side effects:
+            Creates and modifies the attribute called info.
         """
-        if not isinstance(employee, (Employee, int)):
+        if not isinstance(employee, (Employee, str)):
             raise TypeError("The employee arg should be an Employee or an int.")
-        if isinstance(employee, int) and ({name, gender, dob, email, phone
+        if isinstance(employee, str) and ({name, gender, dob, email, phone
                 , address, position, department, salary} & BAD_VALUES):
             raise ValueError("If an ID is given then the other attributes must"
                 "be non-empty.")
@@ -58,8 +63,7 @@ class Employee:
             self.info = employee.info.copy()
             return
         self.info = {
-            "id":employee
-            ,"name":name
+            "name":employee
             ,"gender":gender
             ,"dob":dob
             ,"email":email
@@ -69,3 +73,51 @@ class Employee:
             ,"department":department
             ,"salary":salary
         }
+
+class Company:
+    """Represents the people in a company.
+    
+    Primary author: ?
+    
+    Attributes:
+        PLACEHOLDER
+        employees_file (str): A path to the JSON which stores all Employee
+            objects.
+        employees (dict of int:Employee): The employee IDs and the corresponding
+            Employee objects.
+    """
+    
+    def __init__(self, employees_file):
+        """PLACEHOLDER
+        
+        Primary author: ?
+        """
+        # PLACEHOLDER
+        self.employees_file = employees_file
+        self.employees = {}
+    
+    def add_employee(self, id, employee, update=False):
+        """Adds an Employee to the dictionary of employees.
+        PLACEHOLDER
+        
+        Primary author: ?
+        
+        Args:
+            id (int): The ID of the employee.
+            employee (Employee): The employee's corresponding Employee object.
+            update (bool): If update is False, then adding an existing ID will
+                fail. If update is True, then the Employee tied to the ID will
+                be overwritten.
+        """
+        # PLACEHOLDER
+    
+    def write_employees_json(self, file, employees, *, add_only=True):
+        """Writes the information of Employees to a file.
+        
+        Primary author: Gene Yu
+        
+        Args:
+            employees (list of Employee): Any combination of employees in the
+                employees dict.
+        """
+        # PLACEHOLDER
