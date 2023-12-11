a6dcd2b7ae74833cc51d1274de3304eed410c205
Steve Tanekeu <stanekeu@umd.edu>
2023-12-11 18:49:32 -0500

    some changes

--- employee_database.py
+++ employee_database.py
@@ -350,16 +350,13 @@ class Company():
 
             if match:
                 matching_employees.append(employee)
-<<<<<<< HEAD
                 
             print("Matching Employees:")
             for employee in matching_employees:
                 print(employee)    
             return
-=======
                    
         return matching_employees
->>>>>>> 8b0d18ef1b36447f4a2bf9a421c277cd5861223f
 
     def edit_employee(self, employee_id):
         if employee_id not in self.employees:
@@ -372,6 +369,8 @@ class Company():
         for attr, value in employee.to_dict().items():
             updated_value = input(f"Enter updated {attr} (press Enter to skip): ") or value
             updated_attributes[attr] = updated_value
+
+        self.employees[employee_id] = Employee(updated_attributes)
         print(f"Employee with ID {employee_id} updated successfully.")
 
     def add_manager(self, name):
@@ -501,12 +500,6 @@ def main():
             last_name = input("Enter employee's last name (leave empty if not specified): ")
             department = input("Enter employee's department (leave empty if not specified): ")
             matching_employees = com.search_employee(first_name, last_name, department)
-            if matching_employees:   
-                print("Matching Employees:")
-                for employee in matching_employees:
-                    print(employee) 
-            else:
-                print("No employees found with the specified criteria.")
         elif answer == 11:
             print("Thank you for using the Employee Management Data Center")
             break
